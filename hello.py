de0d54ec80002649bd1bd40fa32af69e76189a1a
luojiyin <luojiyin@hotmail.com>
2016-12-21 06:30:40 -0800

    finish validators

--- hello.py
+++ hello.py
@@ -1,29 +1,31 @@
-from flask import Flask , render_template
+from flask import Flask, render_template
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
-from flask.ext.moment import Moment
-from datetime import datetime
+from flask_moment import Moment
+from flask_wtf import Form
+from  wtforms import StringField, SubmitField
+from  wtforms.validators import Required
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
 
 manager = Manager(app)
-bootstrap = Bootstrap(app)
+bootstrap =Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/')
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data=''
+    return render_template('index.html', form=form, name=name)
 
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name)
 
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
-
-@app.errorhandler(500)
-def internal_server_error(e):
-    return render_template('500.html'),500
-
-if __name__ =='__main__':
+if __name__=='__main__':
     manager.run()
